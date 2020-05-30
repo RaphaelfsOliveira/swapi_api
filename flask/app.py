35ab1714ac1edf3fe2d6c2286b722431d176cb32
Raphael Oliveira <raphaelbrf@gmail.com>
2020-05-30 17:24:28 -0300

    - html base - view inicial (em desenvolvimento)

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from markupsafe import escape
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'test index'
+    context = {
+        'name': 'Raphael',
+    }
+    return render_template('base.html', **context)
