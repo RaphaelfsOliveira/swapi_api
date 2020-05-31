061b2fbd3b3ff84b6c5611c2ea5faea7c2ab88d3
Raphael Oliveira <raphaelbrf@gmail.com>
2020-05-31 14:04:18 -0300

    - paginação

--- flask/app.py
+++ flask/app.py
@@ -8,17 +8,26 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def IndexView():
     root_api = requests.get(endpoint)
+    url = '{}{}'.format(endpoint, 'people/')
 
     if request.method == 'POST':
-        if request.form:
+        print(
+            request.form
+        )
+        if 'select_category' in request.form:
             url_category = request.form['select_category']
             response = requests.get(url_category)
+        elif 'pagination' in request.form:
+            page = request.form['pagination']
+            response = requests.get(page)
+        else:
+            response = requests.get(url)
     else:    
-        url = '{}{}'.format(endpoint, 'people/')
         response = requests.get(url)
 
     context = {
         'data': response.text,
-        'root_api': root_api.json()
+        'root_api': root_api.json(),
+        'pages': response.json(),
     }
     return render_template('index.html', **context)
